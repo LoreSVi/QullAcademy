95e273c57556782c2a4d85f9217aae7957be39bd
ecos2021 <info@cabanasecosdelvalle.com.ar>
2023-10-29 16:03:28 -0300

    cursos_whatsapp_scripts

--- js/formulario.js
+++ js/formulario.js
@@ -5,7 +5,7 @@ const handleOnClick = ()=>{
     const telefono = document.getElementById('email')
     const email = document.getElementById('phone')
     console.log('llamando boton mensaje'+ consulta)
-    window.open(`https://api.whatsapp.com/send?phone=543546520243&text=Consulta desde el Formulario la Web:
+    window.open(`https://api.whatsapp.com/send?phone=543546520243&text=Consulta desde el Formulario Web:
     ${'%0A'}Nombre: ${nombre.value}
     ${'%0A'}Telefono: ${telefono.value}
     ${'%0A'}Email: ${email.value}
