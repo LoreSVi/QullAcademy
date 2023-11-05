04d2fd6cf110ddd5ab3aaddc60ea63609088401a
ecos2021 <info@cabanasecosdelvalle.com.ar>
2023-11-05 18:54:51 -0300

    fomulario_de_contacto

--- js/formulario.js
+++ js/formulario.js
@@ -2,8 +2,8 @@
 const handleOnClick = ()=>{
     const consulta = document.getElementById('message')
     const nombre = document.getElementById('name')
-    const telefono = document.getElementById('email')
-    const email = document.getElementById('phone')
+    const email = document.getElementById('email')
+    const telefono = document.getElementById('phone')
     console.log('llamando boton mensaje'+ consulta)
     window.open(`https://api.whatsapp.com/send?phone=543546520243&text=Consulta desde el Formulario Web:
     ${'%0A'}Nombre: ${nombre.value}
